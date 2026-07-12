aafaf9f7a6e323ff86d149459544a12829b71a9c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,61 +1,57 @@
-output "backup_policy_vms" {
-  description = "All backup_policy_vm resources"
-  value       = azurerm_backup_policy_vm.backup_policy_vms
-}
 output "backup_policy_vms_backup" {
-  description = "List of backup values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.backup]
+  description = "Map of backup values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.backup }
 }
 output "backup_policy_vms_consistency_type" {
-  description = "List of consistency_type values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.consistency_type]
+  description = "Map of consistency_type values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.consistency_type }
 }
 output "backup_policy_vms_instant_restore_resource_group" {
-  description = "List of instant_restore_resource_group values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.instant_restore_resource_group]
+  description = "Map of instant_restore_resource_group values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.instant_restore_resource_group }
 }
 output "backup_policy_vms_instant_restore_retention_days" {
-  description = "List of instant_restore_retention_days values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.instant_restore_retention_days]
+  description = "Map of instant_restore_retention_days values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.instant_restore_retention_days }
 }
 output "backup_policy_vms_name" {
-  description = "List of name values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.name]
+  description = "Map of name values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.name }
 }
 output "backup_policy_vms_policy_type" {
-  description = "List of policy_type values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.policy_type]
+  description = "Map of policy_type values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.policy_type }
 }
 output "backup_policy_vms_recovery_vault_name" {
-  description = "List of recovery_vault_name values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.recovery_vault_name]
+  description = "Map of recovery_vault_name values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.recovery_vault_name }
 }
 output "backup_policy_vms_resource_group_name" {
-  description = "List of resource_group_name values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.resource_group_name]
+  description = "Map of resource_group_name values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.resource_group_name }
 }
 output "backup_policy_vms_retention_daily" {
-  description = "List of retention_daily values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.retention_daily]
+  description = "Map of retention_daily values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_daily }
 }
 output "backup_policy_vms_retention_monthly" {
-  description = "List of retention_monthly values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.retention_monthly]
+  description = "Map of retention_monthly values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_monthly }
 }
 output "backup_policy_vms_retention_weekly" {
-  description = "List of retention_weekly values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.retention_weekly]
+  description = "Map of retention_weekly values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_weekly }
 }
 output "backup_policy_vms_retention_yearly" {
-  description = "List of retention_yearly values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.retention_yearly]
+  description = "Map of retention_yearly values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_yearly }
 }
 output "backup_policy_vms_tiering_policy" {
-  description = "List of tiering_policy values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.tiering_policy]
+  description = "Map of tiering_policy values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.tiering_policy }
 }
 output "backup_policy_vms_timezone" {
-  description = "List of timezone values across all backup_policy_vms"
-  value       = [for k, v in azurerm_backup_policy_vm.backup_policy_vms : v.timezone]
+  description = "Map of timezone values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.timezone }
 }
 

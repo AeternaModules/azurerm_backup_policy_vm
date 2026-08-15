559130f02eb1674c642b1446842e5769012e366b
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:14:15 +0100

    Update to schema version v5.0.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "backup_policy_vms_id" {
 }
 output "backup_policy_vms_backup" {
   description = "Map of backup values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
-  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.backup if v.backup != null && length(v.backup) > 0 }
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => one(v.backup) if v.backup != null && length(v.backup) > 0 }
 }
 output "backup_policy_vms_consistency_type" {
   description = "Map of consistency_type values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
@@ -12,7 +12,7 @@ output "backup_policy_vms_consistency_type" {
 }
 output "backup_policy_vms_instant_restore_resource_group" {
   description = "Map of instant_restore_resource_group values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
-  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.instant_restore_resource_group if v.instant_restore_resource_group != null && length(v.instant_restore_resource_group) > 0 }
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => one(v.instant_restore_resource_group) if v.instant_restore_resource_group != null && length(v.instant_restore_resource_group) > 0 }
 }
 output "backup_policy_vms_instant_restore_retention_days" {
   description = "Map of instant_restore_retention_days values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
@@ -36,23 +36,23 @@ output "backup_policy_vms_resource_group_name" {
 }
 output "backup_policy_vms_retention_daily" {
   description = "Map of retention_daily values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
-  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_daily if v.retention_daily != null && length(v.retention_daily) > 0 }
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => one(v.retention_daily) if v.retention_daily != null && length(v.retention_daily) > 0 }
 }
 output "backup_policy_vms_retention_monthly" {
   description = "Map of retention_monthly values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
-  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_monthly if v.retention_monthly != null && length(v.retention_monthly) > 0 }
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => one(v.retention_monthly) if v.retention_monthly != null && length(v.retention_monthly) > 0 }
 }
 output "backup_policy_vms_retention_weekly" {
   description = "Map of retention_weekly values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
-  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_weekly if v.retention_weekly != null && length(v.retention_weekly) > 0 }
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => one(v.retention_weekly) if v.retention_weekly != null && length(v.retention_weekly) > 0 }
 }
 output "backup_policy_vms_retention_yearly" {
   description = "Map of retention_yearly values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
-  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.retention_yearly if v.retention_yearly != null && length(v.retention_yearly) > 0 }
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => one(v.retention_yearly) if v.retention_yearly != null && length(v.retention_yearly) > 0 }
 }
 output "backup_policy_vms_tiering_policy" {
   description = "Map of tiering_policy values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
-  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => v.tiering_policy if v.tiering_policy != null && length(v.tiering_policy) > 0 }
+  value       = { for k, v in azurerm_backup_policy_vm.backup_policy_vms : k => one(v.tiering_policy) if v.tiering_policy != null && length(v.tiering_policy) > 0 }
 }
 output "backup_policy_vms_timezone" {
   description = "Map of timezone values across all backup_policy_vms, keyed the same as var.backup_policy_vms"
